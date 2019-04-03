08925c7c05f93a37ec0d504c0fbca5b56689fc07
yu.cr <cr.yu@cyberinsight.com>
2019-04-03 17:47:46 +0800

    list and dict demo

---
+++ basic/dict.py
@@ -0,0 +1,12 @@
+d1={"apple":50,"orange":20,"pear":10}
+
+for key in d1:
+     print(key) #Not necessarily in order
+
+for value in d1.values():
+    print(value)
+
+for item in d1.items():
+    print(item)
+
+
---
+++ basic/list.py
@@ -0,0 +1,15 @@
+
+l1=["helen","is","a","good","student"]
+
+print(l1[0::2])
+print(l1[-1::-2])
+
+l1.append('!')
+print(l1)
+
+l1.insert(0,"Do you know Helen?")
+print(l1)
+
+for i  in l1:
+    print(i,"#")
+
